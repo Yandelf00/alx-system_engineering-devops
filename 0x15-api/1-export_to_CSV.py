ca7978660dd821e7be2a8d0ef382e89950f25242
Yandelf00 <zakaria.afyf00@gmail.com>
2024-01-02 19:34:07 +0100

    add

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(url + f"/{sys.argv[1]}")
+    todos = requests.get(url + f"/{sys.argv[1]}" + "/todos")
+    if user.status_code != 200 or todos.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        sys.exit(1)
+    user_data = user.json()
+    todos_data = todos.json()
+    csv_filename = f"{sys.argv[1]}.csv"
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for todo in todos_data:
+            csv_writer.writerow([{user_data['id']},
+                                user_data['username'],
+                                todo['completed'],
+                                todo['title']])
