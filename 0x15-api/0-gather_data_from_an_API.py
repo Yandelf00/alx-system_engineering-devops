67b09c463a8e33a79627345a3113295b1bf844d4
Yandelf00 <zakaria.afyf00@gmail.com>
2024-01-02 18:37:25 +0100

    add

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,17 +3,18 @@
 import requests
 import sys
 
-url = "https://jsonplaceholder.typicode.com/users"
-user = requests.get(url + f"/{sys.argv[1]}")
-todos = requests.get(url + f"/{sys.argv[1]}" + "/todos")
-total = 0
-comp = 0
-for el in todos.json():
-    if el['completed'] is True:
-        comp += 1
-    total += 1
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(url + f"/{sys.argv[1]}")
+    todos = requests.get(url + f"/{sys.argv[1]}" + "/todos")
+    total = 0
+    comp = 0
+    for el in todos.json():
+        if el['completed'] is True:
+            comp += 1
+        total += 1
 
-print(f"Employee {user.json()['name']} is done with tasks({comp}/{total})")
-for el in todos.json():
-    if el['completed'] is True:
-        print("\t" + " " + f"{el['title']}")
+    print(f"Employee {user.json()['name']} is done with tasks({comp}/{total})")
+    for el in todos.json():
+        if el['completed'] is True:
+            print("\t" + " " + f"{el['title']}")
