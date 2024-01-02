7a74f1eb6bf109922aa5df5279d915d61ee1d498
Yandelf00 <zakaria.afyf00@gmail.com>
2024-01-02 18:28:43 +0100

    add

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,12 +8,12 @@ user = requests.get(url + f"/{sys.argv[1]}")
 todos = requests.get(url + f"/{sys.argv[1]}" + "/todos")
 total = 0
 comp = 0
-for el in todos.json() :
-    if el['completed'] == True :
+for el in todos.json():
+    if el['completed'] is True:
         comp += 1
     total += 1
 
 print(f"Employee {user.json()['name']} is done with tasks({comp}/{total})")
-for el in todos.json() :
-    if el['completed'] == True :
+for el in todos.json():
+    if el['completed'] is True:
         print("\t" + " " + f"{el['title']}")
