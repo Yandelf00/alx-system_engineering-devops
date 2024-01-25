e01c3968fadf2e21b246f5645398a5409b2e30f9
Yandelf00 <zakaria.afyf00@gmail.com>
2024-01-26 00:22:02 +0100

    add

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,13 @@
-exec { 'update hard limit':
-  command => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 4096/' /etc/security/limits.conf",
-  provider => "shell",
+# Enable the user holberton to login and open files without error.
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'update soft limit':
-  command => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 4096/' /etc/security/limits.conf",
-  provider => "shell",
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
