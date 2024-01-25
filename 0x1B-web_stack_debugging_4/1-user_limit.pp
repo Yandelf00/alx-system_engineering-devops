b5b29f3a928755669a5ecfeb8abac9bfc753088b
Yandelf00 <zakaria.afyf00@gmail.com>
2024-01-26 00:16:21 +0100

    add

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,9 @@
-# give holberton more action mskin
-exec {'update hard limit':
-  command => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 4096' /etc/security/limits.conf",
+exec { 'update hard limit':
+  command => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 4096/' /etc/security/limits.conf",
   provider => "shell",
 }
 
-exec {'update soft limit':
-  command => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 4096' /etc/security/limits.conf",
+exec { 'update soft limit':
+  command => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 4096/' /etc/security/limits.conf",
   provider => "shell",
 }
